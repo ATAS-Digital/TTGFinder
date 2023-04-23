c1315eee376635ef60bad473ae0a923fa76ce49e
AnesVijay <2011andreyka@gmail.com>
2023-04-23 19:33:06 +0500

    changed logic of command switching

---
+++ src/main/java/ru/atas/TRPfinder/Bot/Commands/DefaultAnswer.java
@@ -0,0 +1,19 @@
+package ru.atas.TRPfinder.Bot.Commands;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import ru.atas.TRPfinder.Bot.CommandInterface;
+
+@Component
+public class DefaultAnswer implements CommandInterface {
+
+    @Override
+    public SendMessage doAction(Update update) {
+        long chatId = update.getMessage().getChatId();
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId);
+        message.setText("Извините, я не знаю такой команды. :(\n\nПопробуйте написать команду из доступных.");
+        return message;
+    }
+}
