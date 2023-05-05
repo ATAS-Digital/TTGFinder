384aac11a01a463852600e612c5468938d9c1672
Etien <stepan.filippov@yandex.ru>
2023-05-06 00:05:10 +0500

    added method to get game by name

--- src/main/java/ru/atas/TRPfinder/Repositories/GameEventRepository.java
+++ src/main/java/ru/atas/TRPfinder/Repositories/GameEventRepository.java
@@ -14,4 +14,9 @@ public interface GameEventRepository extends CrudRepository<GameEvent, Long> {
     @Query(value = "INSERT INTO GAME_EVENT (GAME_DATE, GAME_NAME, GAME_PLACE, DESCRIPTION) \n"+
     "VALUES (?1, ?2, ?3, ?4)", nativeQuery = true)
     void addNewGame(ZonedDateTime time, String name, String place, String description);
+
+    @Transactional
+    @Query(value = "SELECT * FROM GAME_EVENT\n" +
+            "WHERE GAME_NAME = (?1)", nativeQuery = true)
+    GameEvent getGameByName(String name);
 }
